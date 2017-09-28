e1783190abd13d392ebb30ab5b12c9bd2a1ea2f0
Ami Rahav <arahav@boundlessgeo.com>
2017-09-28 15:26:11 -0400

    get_geofence_rules should return an empty array on failure

--- geonode/security/models.py
+++ geonode/security/models.py
@@ -538,7 +538,7 @@ def get_geofence_rules(workspace=None, layer=None, output_type='xml'):
             return resp.content
     else:
         logger.warning("Could not get rule from GeoFence")
-    return None
+    return []
 
 
 def delete_geofence_rule(id):
